de79c7457b01696a527418e47420ec19884ace96
Matthew Gadd <choco.9@hotmail.co.uk>
2026-05-15 15:35:25 +0100

    adding a header and bootstrap

---
+++ app/header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Head from "next/head";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Header() {
+  return (
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Header from "./header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +16,7 @@ export default function RootLayout({
       lang="en"
       className={`h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col"><Header/>{children}</body>
     </html>
   );
 }
